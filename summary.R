25a926e0c1f941b2717155b0f711c7134c7f33c4
Rizkika Ramadhanti <kika3431@uw.edu>
2024-05-29 01:55:20 -0700

    Update summary.R

--- summary.R
+++ summary.R
@@ -11,6 +11,40 @@ total_prison_pop <- sum(prison_data$total_prison_pop, na.rm = TRUE)
 total_female_prison_pop <- sum(prison_data$female_prison_pop, na.rm = TRUE)
 total_male_prison_pop <- sum(prison_data$male_prison_pop, na.rm = TRUE)
 
+# Total Population by Race
+total_black_prison_pop <- sum(prison_data$black_prison_pop, na.rm = TRUE)
+total_white_prison_pop <- sum(prison_data$white_prison_pop, na.rm = TRUE)
+total_latinx_prison_pop <- sum(prison_data$latinx_prison_pop, na.rm = TRUE)
+total_aapi_prison_pop <- sum(prison_data$aapi_prison_pop, na.rm = TRUE)
+
+# Average Prison Population by County
+avg_prison_pop_by_county <- prison_data %>%
+  group_by(county_name) %>%
+  summarize(avg_prison_pop = mean(total_prison_pop, na.rm = TRUE))
+
+# Top 5 Counties by Total Prison Population
+top_5_counties_by_prison_pop <- prison_data %>%
+  group_by(county_name) %>%
+  summarize(total_prison_pop = sum(total_prison_pop, na.rm = TRUE)) %>%
+  arrange(desc(total_prison_pop)) %>%
+  slice(1:5)
+
+# Change in Total Prison Population Over Time
+change_in_prison_pop_over_time <- prison_data %>%
+  group_by(year) %>%
+  summarize(total_prison_pop = sum(total_prison_pop, na.rm = TRUE))
+
+# Total Urban vs. Rural Prison Population
+total_urban_prison_pop <- prison_data %>%
+  filter(urbanicity == "urban") %>%
+  summarize(total_prison_pop = sum(total_prison_pop, na.rm = TRUE)) %>%
+  pull(total_prison_pop)
+
+total_rural_prison_pop <- prison_data %>%
+  filter(urbanicity == "rural") %>%
+  summarize(total_prison_pop = sum(total_prison_pop, na.rm = TRUE)) %>%
+  pull(total_prison_pop)
+
 # Create a list of summary statistics
 summary_stats <- list(
   total_states = total_states,
@@ -18,5 +52,17 @@ summary_stats <- list(
   total_records = total_records,
   total_prison_pop = total_prison_pop,
   total_female_prison_pop = total_female_prison_pop,
-  total_male_prison_pop = total_male_prison_pop
+  total_male_prison_pop = total_male_prison_pop,
+  total_black_prison_pop = total_black_prison_pop,
+  total_white_prison_pop = total_white_prison_pop,
+  total_latinx_prison_pop = total_latinx_prison_pop,
+  total_aapi_prison_pop = total_aapi_prison_pop,
+  avg_prison_pop_by_county = avg_prison_pop_by_county,
+  top_5_counties_by_prison_pop = top_5_counties_by_prison_pop,
+  change_in_prison_pop_over_time = change_in_prison_pop_over_time,
+  total_urban_prison_pop = total_urban_prison_pop,
+  total_rural_prison_pop = total_rural_prison_pop
 )
+
+# Print the new summary statistics
+print(new_summary_stats)
